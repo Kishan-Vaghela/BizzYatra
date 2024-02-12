285738e1724644e94638df44b898234db2a35af3
Kishan-Vaghela <vaghelakishan301@gmail.com>
2024-02-12 12:10:20 +0530

    Commiting the files

---
+++ js/external.js
@@ -0,0 +1,19 @@
+document.write("External Javascript file are used!!");
+//This is a simple javascript program that displays "Hello World" on the browser window using external
+//implemting the javascript variable..
+
+x =5;
+y =6;
+z= x+y;
+
+document.write(z); 
+document.write("<br>") //using document write method to display it in HTML
+const price1 = 5;
+const price2 = 6;
+let total = price1 + price2;
+document.write(total);document.write("<br>") 
+var c = "50";
+var a = '10';
+var b = 'hello';
+var z = x+y+total+b;
+document.write(z);document.write("<br>") 
